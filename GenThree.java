45ca6d7a0c12435a7627767c21f92947fd03e60a
YgorKa97 <154316908+YgorKa97@users.noreply.github.com>
2023-12-22 15:04:38 +0200

    GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,19 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		//user input for lower and upper bound.
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        // Three random numbers
+        int rand1 = a + (int) (Math.random() * (b-a));
+        int rand2 = a + (int) (Math.random() * (b-a));
+        int rand3 = a + (int) (Math.random() * (b-a));
+        // print random numbers
+        System.out.println("First random number is: " + rand1);
+        System.out.println("Second random number is: " + rand2);
+        System.out.println("Third random number is: " + rand3);
+        // find the minimal number
+        int min = Math.min(Math.min(rand1,rand2),rand3);
+        System.out.println("Minimal number is: " + min);	
 	}
 }
