1343d410401dd2648a7827cf94b2cb2ab39c8aeb
YgorKa97 <154316908+YgorKa97@users.noreply.github.com>
2023-12-22 13:36:11 +0200

    LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -5,5 +5,19 @@
  * Treats the three arguments as well as the computed value as double values
  */
 public class LinearEq {
-	// Put your code here
+	        // User input of a,b,c
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = Double.parseDouble(args[2]);
+        // Print out the equation
+        System.out.println(a + " * x + " + b + " = " + c);
+        //assume that a is not 0
+        if (a != 0) {
+            double x = (c - b) / a;
+            System.out.println("x = " + x);
+            // if a = 0, print Error
+        } else {
+            System.out.println("ERROR");
+        }
+
 }
