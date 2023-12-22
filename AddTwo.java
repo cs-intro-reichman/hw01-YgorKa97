43fe9d25557f1249e192ce522c795b71049c550b
YgorKa97 <154316908+YgorKa97@users.noreply.github.com>
2023-12-22 16:26:16 +0200

    AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,7 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
+		//put your code here
 		// User input
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
