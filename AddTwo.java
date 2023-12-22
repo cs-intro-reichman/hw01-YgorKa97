c28a08ba91c934bbc4a78edaf8123abd62aa5771
YgorKa97 <154316908+YgorKa97@users.noreply.github.com>
2023-12-22 12:25:55 +0200

    AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,10 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
+		// Put your code here
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int ans = a + b;
+		System.out.println(a + " + " + b + " = " + ans);
 	}
 }
