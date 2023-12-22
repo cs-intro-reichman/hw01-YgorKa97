e261fd0ba0abfc86e8fba64c4ceb9b60219de55c
YgorKa97 <154316908+YgorKa97@users.noreply.github.com>
2023-12-22 14:00:59 +0200

    Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -4,7 +4,21 @@
  * Write a program that tests if three given integers form a triangle.
  */ 
 public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+ 	public static void main(String[] args) {
+		//user input
+       	 	int a = Integer.parseInt(args[0]);
+        	int b = Integer.parseInt(args[1]);
+        	int c = Integer.parseInt(args[2]);
+		//boolean 
+       		boolean triangle = true;
+        if ((a + b) < c || (a + c) < b || (b + c) < a){
+		//will print false
+            System.out.println(!triangle);
+        }else{
+		//will print true
+            System.out.println(triangle);
+        }
+
+    }
+	
 }
